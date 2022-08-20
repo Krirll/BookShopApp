4fad3569c82c640b7ab0d1125a11adcdb89a2a36
Krirll <balalaev20022@gmail.com>
2022-08-20 15:19:23 +0300

    changed the impl with KoinViewModel

--- app/src/main/java/ru/krirll/testtask/presentation/fragments/BookDetailsFragment.kt
+++ app/src/main/java/ru/krirll/testtask/presentation/fragments/BookDetailsFragment.kt
@@ -1,13 +1,11 @@
 package ru.krirll.testtask.presentation.fragments
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
@@ -15,36 +13,24 @@ import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.launch
-import ru.krirll.testtask.MainApplication
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.krirll.testtask.R
 import ru.krirll.testtask.databinding.FragmentBookDetailsBinding
 import ru.krirll.testtask.presentation.listAdapters.ImagesAdapter
-import ru.krirll.testtask.presentation.viewModels.uiState.BookDetailsUiState
 import ru.krirll.testtask.presentation.viewModels.BookDetailsViewModel
-import ru.krirll.testtask.presentation.viewModels.ViewModelFactory
-import javax.inject.Inject
+import ru.krirll.testtask.presentation.viewModels.uiState.BookDetailsUiState
 
 class BookDetailsFragment : Fragment() {
 
     private val args by navArgs<BookDetailsFragmentArgs>()
 
-    @Inject
-    lateinit var viewModelFactory: ViewModelFactory
-
-    private val bookDetailsViewModel by lazy {
-        ViewModelProvider(this, viewModelFactory)[BookDetailsViewModel::class.java]
-    }
+    private val bookDetailsViewModel by viewModel<BookDetailsViewModel>()
 
     private var _viewBinding: FragmentBookDetailsBinding? = null
     private val viewBinding: FragmentBookDetailsBinding
         get() = _viewBinding ?: throw RuntimeException("FragmentBookDetailsBinding == null")
 
     private var similarAdapter: ImagesAdapter? = null
-
-    override fun onAttach(context: Context) {
-        (requireActivity().application as MainApplication).component.inject(this)
-        super.onAttach(context)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
