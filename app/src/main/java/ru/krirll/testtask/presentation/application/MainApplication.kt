b15fe64e2e3b9406428e8f2d6ce2be0192435ee1
Krirll <balalaev20022@gmail.com>
2022-08-20 15:17:56 +0300

    override onCreate for startKoin

--- app/src/main/java/ru/krirll/testtask/presentation/application/MainApplication.kt
+++ app/src/main/java/ru/krirll/testtask/presentation/application/MainApplication.kt
@@ -1,11 +1,21 @@
-package ru.krirll.testtask
+package ru.krirll.testtask.presentation.application
 
 import android.app.Application
-import ru.krirll.testtask.di.DaggerApplicationComponent
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import ru.krirll.testtask.di.appModule
+import ru.krirll.testtask.di.dataModule
+import ru.krirll.testtask.di.domainModule
 
 class MainApplication: Application() {
 
-    val component by lazy {
-        DaggerApplicationComponent.create()
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(appModule, dataModule, domainModule)
+        }
     }
 }
